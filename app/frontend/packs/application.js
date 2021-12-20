e33594c254da2dc86cc38a39dbc7f49c095328e4
Greg <GregJieNing@gmail.com>
2021-12-21 02:54:36 +0800

    set boostrap devise template

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,13 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-// Add this line
-import '../js/bootstrap_js_files.js'
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+import '../stylesheets/application'
+import 'bootstrap/dist/js/bootstrap'
 
 Rails.start()
 Turbolinks.start()
